64c35f59d5de7966dc8ecf3a0d1f451e59191280
Vincent Bezold <vincent.bezold@web.de>
2022-04-26 23:09:19 +0200

    init

---
+++ Part1Addition.m
@@ -0,0 +1,11 @@
+% Addition
+
+er = 1:10;
+bbe = 1:10;
+ans = test(er,bbe);
+
+
+
+function c = test(a,b)
+ c = a + b;
+end
---
+++ Part2PlotRandom.m
@@ -0,0 +1,8 @@
+close all
+clear all
+clc
+
+numbers = 10;
+randomNumbers = rand(numbers,1);
+
+plot(randomNumbers, 'x')
