8ca37d36485d81d93155f2a67f6e286de1641a3b
Vincent Bezold <vincent.bezold@web.de>
2022-05-19 18:38:06 +0200

    finished all missing excerises for the weekend; reorganized the folders

--- dataCreater2.m
+++
@@ -1,7 +0,0 @@
-x = 1:100;
-A = cos(2*pi*0.05*x+2*pi*rand) + 0.5*randn(1,100);
-[B,window] = smoothdata(A,'gaussian');
-% C = smoothdata(A,'gaussian',20);
-C = smoothdata(A, 200);
-plot(x,A,'-o',x,C,'-x')
-legend('Small Window','Large Window')
--- dayOne/DataCreator.m
+++ dayOne/DataCreator.m
@@ -7,15 +7,17 @@ x = -10:0.05:10;
 y = exp(x/10);
 yy = 10 * 2.^(x/7);
 yyy = -1*(x-4).^2 + 5;
-yyNoise = wgn(length(yy),1,-5);
-yyyy = sin(x*3)./x;
+yyNoise = wgn(length(yy),1,-6);
+% yyyy = sin(x*3)./x;
 yyyy = 1.5*sin(x);
 % yyyy = -x.^5 + 5* x.^3 - 4*x
-% yyyy = yyyy + yyNoise';
+yyyy = yyyy + yyNoise';
+yyyy(132) = yyyy(132) + 4.5;
+yyyy(242) = yyyy(242) - 5.25;
 % yy = round(yy);
 M = [x; yyyy];
-% plot(x,yyyy,'.')
-% hold on
-% plot(x,movmean(yyyy, 10))
+plot(x,yyyy,'.')
+hold on
+plot(x,movmean(yyyy, 10))
 % disp(1)
-csvwrite('sin.csv',M')
+csvwrite('sinNoisy.csv',M')
